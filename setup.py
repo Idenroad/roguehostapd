b44c3e08fdb5d583bf5c727f0c1006ef276a7c8d
Idenroad <150827422+Idenroad@users.noreply.github.com>
2025-12-22 22:20:48 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -30,10 +30,63 @@ URL = 'https://github.com/idenroad/roguehostapd'
 AUTHOR = 'idenroad (fork), Anakin (original)'
 PYTHON_REQUIRES = '>=3.8'
 
+# Try to import build helpers; if unavailable, fall back to a pure-Python install
+buildcommon = None
+buildexception = None
 try:
     import roguehostapd.buildutil.buildcommon as buildcommon
     import roguehostapd.buildutil.buildexception as buildexception
-    EXT_MODULE = buildcommon.get_extension_module()
+except Exception:
+    buildcommon = None
+    buildexception = None
+
+if buildcommon is not None:
+    try:
+        EXT_MODULE = buildcommon.get_extension_module()
+        setup(
+            name=NAME,
+            packages=PACKAGES,
+            package_dir=PACKAGE_DIR,
+            package_data=PACKAGE_DATA,
+            version=VERSION,
+            description=DESCRIPTION,
+            url=URL,
+            author=AUTHOR,
+            python_requires=PYTHON_REQUIRES,
+            install_requires=[],
+            zip_safe=False,
+            cmdclass={
+                'build_ext': build_ext,
+                'install': install
+            },
+            ext_modules=EXT_MODULE,
+            classifiers=[
+                'Development Status :: 4 - Beta',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: BSD License',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.9',
+                'Programming Language :: Python :: 3.10',
+                'Programming Language :: Python :: 3.11',
+                'Programming Language :: Python :: 3.12',
+                'Programming Language :: Python :: 3.13',
+                'Operating System :: POSIX :: Linux',
+            ],
+        )
+    except Exception as exc:
+        # If the build helpers signalled a missing shared lib, print a readable message
+        if buildexception is not None and isinstance(exc, buildexception.SharedLibMissError):
+            exobj = exc
+            print("[" + RED + "!" + WHITE + "] " +
+                  ("The development package for " + exobj.libname +
+                   " is missing. Please download it and restart the compilation."
+                   "If you are on Debian-based system: \'apt-get install{}\'.".format(
+                       "".join(" " + package for package in exobj.packages))))
+        else:
+            raise
+else:
+    # No build helpers available; install without compiled extensions
     setup(
         name=NAME,
         packages=PACKAGES,
@@ -46,11 +99,6 @@ try:
         python_requires=PYTHON_REQUIRES,
         install_requires=[],
         zip_safe=False,
-        cmdclass={
-            'build_ext': build_ext,
-            'install': install
-        },
-        ext_modules=EXT_MODULE,
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
@@ -65,12 +113,7 @@ try:
             'Operating System :: POSIX :: Linux',
         ],
     )
-except buildexception.SharedLibMissError as exobj:
-    print("[" + RED + "!" + WHITE + "] " +
-          ("The development package for " + exobj.libname +
-           " is missing. Please download it and restart the compilation."
-           "If you are on Debian-based system: \'apt-get install{}\'.".format(
-               "".join(" " + package for package in exobj.packages))))
-finally:
-    if os.path.isdir('tmp'):
-        shutil.rmtree('tmp')
+
+# cleanup temporary build directory if present
+if os.path.isdir('tmp'):
+    shutil.rmtree('tmp')
